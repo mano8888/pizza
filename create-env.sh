24cf44a4ccbea954e4057480d8cffd2abcad4691
mano8888 <manukambakam.k@gmail.com>
2018-11-01 08:14:35 -0700

    added a new pom file

--- create-env.sh
+++ create-env.sh
@@ -5,9 +5,9 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.gamutkart$i container.."
+    echo "Creating www.hurt$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it --rm nageshvkn/gamutkart-img /bin/bash
-    echo "www.gamutkart$i container has been created!"
+    docker run --name www.hurt$i -d -it --rm nageshvkn/gamutkart-img /bin/bash
+    echo "www.hurt$i container has been created!"
 	echo "=============================="
 done
